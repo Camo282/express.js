e6314779fe7341e7d6d057ead5d9faeaf95e7ef4
Camo282 <camo282@gmail.com>
2022-08-10 11:14:25 -0400

    adding server

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const { notes } = require('./db/db');
+
+/*app.get('/api/db', (req, res) => {
+    res.send('Hello!');
+  });*/
 
 app.listen(3001, () => {
     console.log('API server now on port 3001!');
