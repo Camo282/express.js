383a8d91b981487548392fa690374a0e0e2523ff
Camo282 <camo282@gmail.com>
2022-08-10 14:07:40 -0400

    Add Heroku

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
+//Require dependencies
 const express = require('express');
-const app = express();
-const { notes } = require('./db/db');
+const fs = require('fs');
+const path = require('path');
 
-/*app.get('/api/db', (req, res) => {
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const { notes } = require('./db/db');
+app.get('/api/db', (req, res) => {
     res.send('Hello!');
-  });*/
+  });
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(_dirname));
+
+//Require routes
+require('routes/notesRoutes')(app);
 
 app.listen(3001, () => {
     console.log('API server now on port 3001!');
